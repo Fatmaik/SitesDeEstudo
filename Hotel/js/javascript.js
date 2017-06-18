69ffa71632c96774fd73ce5036e44e87d49b4094
Dionathan <dionathan_bass@hotmail.com>
2017-06-18 18:29:55 -0300

    precos de quartos atualizados

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -1,19 +1,31 @@
 window.onload = function() {
    valorQuartos();
-//    document.getElementById("casal").onmouseout = retirarShadow();
-}
 
-function retirarShadow() {
-    var casal = document.getElementById("casal")
-    casal.onmouseover.style.boxShadow = "0px 0px 0px silver"; 
 }
-
 function valorQuartos() {
     var casal = document.getElementById("casal");
+    var solteiro = document.getElementById("solteiro");
+
     casal.onmouseover = function() {
         this.setAttribute("class", "classShadow");
-       
+        document.getElementById("precoCasal").style.visibility = "visible";
+
     }
+    casal.onmouseleave = function() {
+        this.removeAttribute("class");
+        document.getElementById("precoCasal").style.visibility = "hidden";
+    }
+
+    solteiro.onmouseover = function() {
+        this.setAttribute("class", "classShadow");
+        document.getElementById("precoSolteiro").style.visibility = "visible";
+    }
+
+    solteiro.onmouseleave = function() {
+        this.removeAttribute("class", "classShadow");
+        document.getElementById("precoSolteiro").style.visibility = "hidden";
+    }
+    
      
 }
 
