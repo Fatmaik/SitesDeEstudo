0f3feb833e356b00fb00f6b2332923efb7fbb1f6
matheus <matheusdamian70@gmail.com>
2017-06-19 22:22:45 -0300

    alteração logos

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -1,5 +1,8 @@
 window.onload = function() {
    valorQuartos();
+   Face();
+   Insta();
+   Twitter();
 
 }
 function valorQuartos() {
@@ -24,8 +27,41 @@ function valorQuartos() {
     solteiro.onmouseleave = function() {
         this.removeAttribute("class", "classShadow");
         document.getElementById("precoSolteiro").style.visibility = "hidden";
-    }
-    
-     
+    }  
 }
 
+function Face(){
+    var face = document.getElementById("face");
+
+    face.onmouseover = function(){
+        this.innerHTML = '<img src="css/imagens/LogoFacebook/FaceAmarelo.png">';
+    }
+
+    face.onmouseleave = function(){
+        this.innerHTML = '<img src="css/imagens/LogoFacebook/FaceBranco.png">';
+    }
+}
+
+function Insta(){
+    var insta = document.getElementById("insta");
+    
+    insta.onmouseover = function(){
+        this.innerHTML = '<img src="css/imagens/LogoInsta/InstaAmarelo.png">';
+    }
+
+    insta.onmouseleave = function(){
+        this.innerHTML = '<img src="css/imagens/LogoInsta/InstaBranco.png">';
+    }
+}
+
+function Twitter(){
+    var twitter = document.getElementById("twitter");
+    
+    twitter.onmouseover = function(){
+        this.innerHTML = '<img src="css/imagens/LogoTwitter/TwitterAmarelo.png">';
+    }
+
+    twitter.onmouseleave = function(){
+        this.innerHTML = '<img src="css/imagens/LogoTwitter/TwitterBranco.png">';
+    }
+}
