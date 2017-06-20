d76e0b328e7c446521f58e172266352d6d9863b4
Dionathan <dionathan_bass@hotmail.com>
2017-06-19 22:21:36 -0300

    javascript cuncao de quartos

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -1,6 +1,5 @@
 window.onload = function() {
    valorQuartos();
-
 }
 function valorQuartos() {
     var casal = document.getElementById("casal");
@@ -9,23 +8,26 @@ function valorQuartos() {
     casal.onmouseover = function() {
         this.setAttribute("class", "classShadow");
         document.getElementById("precoCasal").style.visibility = "visible";
+        document.getElementById("precoCasal").setAttribute("class", "classAnimationCasado");
 
     }
     casal.onmouseleave = function() {
         this.removeAttribute("class");
         document.getElementById("precoCasal").style.visibility = "hidden";
+        document.getElementById("precoCasal").removeAttribute("class", "classAnimationCasado");
     }
 
     solteiro.onmouseover = function() {
         this.setAttribute("class", "classShadow");
         document.getElementById("precoSolteiro").style.visibility = "visible";
+        document.getElementById("precoSolteiro").setAttribute("class", "classAnimationSolteiro");
+
     }
 
     solteiro.onmouseleave = function() {
         this.removeAttribute("class", "classShadow");
         document.getElementById("precoSolteiro").style.visibility = "hidden";
-    }
-    
-     
+        document.getElementById("precoSolteiro").removeAttribute("class", "classAnimationSolteiro");
+    } 
 }
 
