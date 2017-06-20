f24ce38cabb6d4fcde60dda06a89d53eea4bbc5a
Dionathan <dionathan_bass@hotmail.com>
2017-06-19 22:39:18 -0300

    teste de bug

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -1,9 +1,5 @@
 window.onload = function() {
    valorQuartos();
-   Face();
-   Insta();
-   Twitter();
-
 }
 function valorQuartos() {
     var casal = document.getElementById("casal");
@@ -13,8 +9,8 @@ function valorQuartos() {
         this.setAttribute("class", "classShadow");
         document.getElementById("precoCasal").style.visibility = "visible";
         document.getElementById("precoCasal").setAttribute("class", "classAnimationCasado");
-
     }
+
     casal.onmouseleave = function() {
         this.removeAttribute("class");
         document.getElementById("precoCasal").style.visibility = "hidden";
@@ -25,7 +21,6 @@ function valorQuartos() {
         this.setAttribute("class", "classShadow");
         document.getElementById("precoSolteiro").style.visibility = "visible";
         document.getElementById("precoSolteiro").setAttribute("class", "classAnimationSolteiro");
-
     }
 
     solteiro.onmouseleave = function() {
@@ -35,38 +30,3 @@ function valorQuartos() {
     } 
 }
 
-function Face(){
-    var face = document.getElementById("face");
-
-    face.onmouseover = function(){
-        this.innerHTML = '<img src="css/imagens/LogoFacebook/FaceAmarelo.png">';
-    }
-
-    face.onmouseleave = function(){
-        this.innerHTML = '<img src="css/imagens/LogoFacebook/FaceBranco.png">';
-    }
-}
-
-function Insta(){
-    var insta = document.getElementById("insta");
-    
-    insta.onmouseover = function(){
-        this.innerHTML = '<img src="css/imagens/LogoInsta/InstaAmarelo.png">';
-    }
-
-    insta.onmouseleave = function(){
-        this.innerHTML = '<img src="css/imagens/LogoInsta/InstaBranco.png">';
-    }
-}
-
-function Twitter(){
-    var twitter = document.getElementById("twitter");
-    
-    twitter.onmouseover = function(){
-        this.innerHTML = '<img src="css/imagens/LogoTwitter/TwitterAmarelo.png">';
-    }
-
-    twitter.onmouseleave = function(){
-        this.innerHTML = '<img src="css/imagens/LogoTwitter/TwitterBranco.png">';
-    }
-}
