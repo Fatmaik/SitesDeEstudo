062a8b68a86425eb9cd9bd1db02dfd8dd17d5555
Dionathan <dionathan_bass@hotmail.com>
2017-06-24 18:15:43 -0300

    footer done

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -2,11 +2,6 @@ $(document).ready(function() {
     valorQuartos();
     trocarFace();
 }) 
-<<<<<<< HEAD
-
-=======
-   
->>>>>>> 7e1f69fa338d8b388ac64283da8e14bbe05883bb
 function valorQuartos() {
     var casal = document.getElementById("casal");
     var solteiro = document.getElementById("solteiro");
@@ -34,11 +29,6 @@ function valorQuartos() {
         document.getElementById("precoSolteiro").style.visibility = "hidden";
         document.getElementById("precoSolteiro").removeAttribute("class", "classAnimationSolteiro");
     } 
-<<<<<<< HEAD
+
 }
 
-
-
-=======
-}
->>>>>>> 7e1f69fa338d8b388ac64283da8e14bbe05883bb
