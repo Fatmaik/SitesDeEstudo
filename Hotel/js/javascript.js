807f9f27362974c1bd7a1fdcce0c030d95203df4
matheus <matheusdamian70@gmail.com>
2017-06-24 17:49:36 -0300

    Lazer

--- Hotel/js/javascript.js
+++ Hotel/js/javascript.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
     valorQuartos();
     trocarFace();
-}) 
-<<<<<<< HEAD
+})
 
-=======
-   
->>>>>>> 7e1f69fa338d8b388ac64283da8e14bbe05883bb
 function valorQuartos() {
     var casal = document.getElementById("casal");
     var solteiro = document.getElementById("solteiro");
@@ -34,11 +30,4 @@ function valorQuartos() {
         document.getElementById("precoSolteiro").style.visibility = "hidden";
         document.getElementById("precoSolteiro").removeAttribute("class", "classAnimationSolteiro");
     } 
-<<<<<<< HEAD
 }
-
-
-
-=======
-}
->>>>>>> 7e1f69fa338d8b388ac64283da8e14bbe05883bb
